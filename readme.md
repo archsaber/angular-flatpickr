d646fc437037072d2e28e21acc423365ef164bef
kremmerseb <sk@localinit.com>
2016-07-05 18:34:27 +0200

    formatting and typo
    Signed-off-by: kremmerseb <sk@localinit.com>

--- readme.md
+++ readme.md
@@ -9,7 +9,7 @@ Currently it has following capabilities
 
 ## Example
 
-* intsall it with `bower install --save angular-flatpickr`
+* install it with `bower install --save angular-flatpickr`
 
 * Add the `ng-flatpickr` module in your app as
 
--- src/ng-flatpickr.js
+++ src/ng-flatpickr.js
@@ -1,38 +1,33 @@
 (function(root, factory) {
-    'use strict';
-    root['angular-flatpickr'] = factory(root.angular, root.flatpickr);
+  'use strict';
+  root['angular-flatpickr'] = factory(root.angular, root.flatpickr);
 }(this, function(angular, flatpickr) {
 
-    'use strict';
-    var ngFlatpickr = angular.module('angular-flatpickr', []);
-    ngFlatpickr.directive('ngFlatpickr', [
+  'use strict';
+  var ngFlatpickr = angular.module('angular-flatpickr', []);
+  ngFlatpickr.directive('ngFlatpickr', [function() {
+    return {
+      restrict : 'A',
+      scope : {
+        fpOpts : '&',
+        fpOnSetup : '&'
+      },
+      link : function(scope, element, iAttrs) {
+        var vp;
+        if (scope.fpOpts()) {
+          vp = flatpickr(element[0], scope.fpOpts());
+        } else {
+          vp = flatpickr(element[0]);
+        }
 
-        function()
-        {
-            return {
-                restrict: 'A',
-                scope: {
-                    fpOpts: '&',
-                    fpOnSetup: '&'
-                },
-                link: function(scope, element, iAttrs) {
-                    var vp;
-                    if (scope.fpOpts()) {
-                        vp = flatpickr(element[0], scope.fpOpts());
-                    } else {
-                        vp = flatpickr(element[0]);
-                    }
+        if (scope.fpOnSetup) {
+          scope.fpOnSetup({
+            fpItem : vp
+          });
+        }
+      }
+    };
+  }]);
 
-                    if (scope.fpOnSetup) {
-                        scope.fpOnSetup({
-                            fpItem: vp
-                        });
-                    }
-                }
-            };
-        },
-    ]);
-
-    return ngFlatpickr;
-
+  return ngFlatpickr;
 }));

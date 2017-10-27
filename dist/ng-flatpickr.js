5044aafe88829b4fe1db7bbf989c2ff7d790f307
opavader <ashish@archsaber.com>
2017-10-27 09:18:22 +0530

    compatiblity fix for flatpickr > 3.0.7

--- dist/ng-flatpickr.js
+++ dist/ng-flatpickr.js
@@ -1,31 +1,40 @@
-(function(root, factory) {
+(function (root, factory) {
   'use strict';
   root['angular-flatpickr'] = factory(root.angular, root.flatpickr);
-}(this, function(angular, flatpickr) {
+}(this, function (angular, flatpickr) {
 
   'use strict';
   var ngFlatpickr = angular.module('angular-flatpickr', []);
-  ngFlatpickr.directive('ngFlatpickr', [function() {
+  ngFlatpickr.directive('ngFlatpickr', [function () {
     return {
       require: 'ngModel',
-      restrict : 'A',
-      scope : {
-        fpOpts : '&',
-        fpOnSetup : '&'
+      restrict: 'A',
+      scope: {
+        fpOpts: '&',
+        fpOnSetup: '&'
       },
-      link : function(scope, element, attrs, ngModel) {
+      link: function (scope, element, attrs, ngModel) {
 
-        var vp = new FlatpickrInstance(element[0], scope.fpOpts());
+        var vp;
+        if (!FlatpickrInstance || !flatpickr) {
+          console.warn('Unable to find any flatpickr installation');
+        }
+        if (FlatpickrInstance) {
+          vp = new FlatpickrInstance(element[0], scope.fpOpts());
+        } else {
+          vp = new flatpickr(element[0], scope.fpOpts());
+        }
+
 
         if (scope.fpOnSetup) {
           scope.fpOnSetup({
-            fpItem : vp
+            fpItem: vp
           });
         }
 
         // destroy the flatpickr instance when the dom element is removed
-        element.on('$destroy',function(){
-            vp.destroy();
+        element.on('$destroy', function () {
+          vp.destroy();
         });
       }
     };

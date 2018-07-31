5ba93d802dea344ed0b88e2644b25d725edb93e1
Simon Dragsbæk Petersen <simon@introdus.dk>
2018-07-31 12:42:06 +0200

    Fix all dependencies and upgrade gulp to be vulnerability free check [90m                                                                                [39m [90m [39m                      === npm audit security report ===                       [90m [39m [90m                                                                                [39m found 0 vulnerabilities  in 5165 scanned packages

--- gulpFile.js
+++ gulpFile.js
@@ -9,45 +9,48 @@ var stylish = require('jshint-stylish');
 var packageJson = require('./package.json');
 
 var distFolder = './dist/';
-var src = (['ng-flatpickr']).map(function (val) {
-    return 'src/' + val + '.js';
+var src = (['ng-flatpickr']).map(function(val) {
+  return 'src/' + val + '.js';
 });
 
 
 //just as indication
-gulp.task('lint', function () {
-    gulp.src(src)
-        .pipe(jshint())
-        .pipe(jshint.reporter(stylish));
+gulp.task('lint', function() {
+  return gulp.src(src)
+    .pipe(jshint())
+    .pipe(jshint.reporter(stylish));
 });
 
 
-gulp.task('uglify', function () {
-    gulp.src(src)
-      .pipe(concat('ng-flatpickr.min.js'))
-      .pipe(sourcemaps.init())
-      .pipe(uglify())
-      .pipe(sourcemaps.write('.'))
-      .pipe(gulp.dest(distFolder));
+gulp.task('uglify', function() {
+  return gulp.src(src)
+    .pipe(concat('ng-flatpickr.min.js'))
+    .pipe(sourcemaps.init())
+    .pipe(uglify())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(distFolder));
 });
 
-gulp.task('concat', function () {
-    gulp.src(src, { base: '.' })
-      .pipe(concat('ng-flatpickr.js'))
-      .pipe(gulp.dest(distFolder));
+gulp.task('concat', function() {
+  return gulp.src(src, {
+      base: '.'
+    })
+    .pipe(concat('ng-flatpickr.js'))
+    .pipe(gulp.dest(distFolder));
 });
 
 
-gulp.task('clean', function (done) {
-     del([distFolder + '/*'],{force:true}, done);
+gulp.task('clean', function() {
+  return del([distFolder + '/*'], {
+    force: true
+  });
 });
 
-gulp.task('build',[ 'clean','lint','uglify', 'concat'], function () {
+gulp.task('build', gulp.series('clean', 'lint', 'uglify', 'concat', function() {
+  var version = packageJson.version;
+  var string = '/** \n* @version ' + version + '\n* @license MIT\n*/\n';
 
-    var version = packageJson.version;
-    var string = '/** \n* @version ' + version + '\n* @license MIT\n*/\n';
-
-    gulp.src(distFolder + '*.js')
-        .pipe(insert.prepend(string))
-        .pipe(gulp.dest(distFolder));
-});
+  return gulp.src(distFolder + '*.js')
+    .pipe(insert.prepend(string))
+    .pipe(gulp.dest(distFolder));
+}));
